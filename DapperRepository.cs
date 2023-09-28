7080831cc8ad045e5cc84aa922c7601c8fa40107
Bithellio <Thomas.bithell@gmail.com>
2023-09-28 13:38:10 +0100

    Add files via upload

---
+++ DapperRepository.cs
@@ -0,0 +1,112 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dapper_Example
+{
+    public class DapperRepository
+    {
+
+        //step 1: install these packages :
+        // - Dapper
+        // -System.Data.SqlClient;
+
+        // if you are running a simple peice of sql with some passed in arguments then do it like this  with no response 
+
+
+        public async Task<bool> RunSqlQuery(long locationId, int employeeId, DateTime timeStamp)
+        {
+            try
+            {
+                using (var connection = new SqlConnection("SQL CONNECTION STRING GOES HERE "))
+                { 
+                    var sql = $"SELECT * FROM <SOME TABLE> WHERE LocationId='{locationId}'" ;
+                    var values = new { Location_ID = locationId, Employee_ID = employeeId, Datetime = timeStamp };
+                    var result = await connection.ExecuteAsync(sql);
+                    return true;
+                }
+            }
+            catch (SqlException ex)
+            {
+                return false;
+            }
+        }
+
+
+
+
+
+        //If you are pulling from a stored Procedure then you would call it with something like this 
+        public async Task<bool> CallStoredProcedure(long locationId, int employeeId, DateTime timeStamp)
+        {
+            try
+            {
+                using (var connection = new SqlConnection("SQL CONNECTION STRING GOES HERE "))
+                {
+                    var procedure = "[stocktake].[PR_Stocktake_CompleteFirstCount]";
+                    var values = new { Location_ID = locationId, Employee_ID = employeeId, Datetime = timeStamp };
+                    var result = await connection.ExecuteAsync(procedure, values, commandType: CommandType.StoredProcedure);
+                    return true;
+                }
+            }
+            catch (SqlException ex)
+            {
+                return false;
+            }
+        }
+
+
+        // if you get rows of data back from sql then make an object that matches the columns and we can return the data as a list of that
+
+
+
+        public async Task<List<returnobject>> RunSqlQuerywithObjectReturned(long locationId, int employeeId, DateTime timeStamp)
+        {
+            try
+            {
+                using (var connection = new SqlConnection("SQL CONNECTION STRING GOES HERE "))
+                {
+                    var sql = $"SELECT * FROM <SOME TABLE> WHERE LocationId='{locationId}'";
+                    var values = new { Location_ID = locationId, Employee_ID = employeeId, Datetime = timeStamp };
+                    var result = await connection.QueryAsync<returnobject>(sql);
+                    return result.ToList();
+                }
+            }
+            catch (SqlException ex)
+            {
+                return null;
+            }
+        }
+
+
+
+
+
+        //If you are pulling from a stored Procedure then you would call it with something like this 
+        public async Task<List<returnobject>> CallStoredProcedureWithObjectReturned (long locationId, int employeeId, DateTime timeStamp)
+        {
+            try
+            {
+                using (var connection = new SqlConnection("SQL CONNECTION STRING GOES HERE "))
+                {
+                    var procedure = "[stocktake].[PR_Stocktake_CompleteFirstCount]";
+                    var values = new { Location_ID = locationId, Employee_ID = employeeId, Datetime = timeStamp };
+                    var result = await connection.QueryAsync<returnobject>(procedure, values, commandType: CommandType.StoredProcedure);
+                    return result.ToList();
+                }
+            }
+            catch (SqlException ex)
+            {
+                return null;
+            }
+        }
+
+
+
+    }
+}
---
+++ returnobject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dapper_Example
+{
+    public class returnobject
+    {
+        public int column1 { get; set; }
+        public string column2 { get; set; }
+        public decimal column3 { get; set; }
+    }
+}
